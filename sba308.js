9d31c80cadaab93eb1d874d2368d4a325d0fec7f
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-03 19:06:53 -0600

    III. Arrays & Control flow - DONE

--- sba308.js
+++ sba308.js
@@ -218,6 +218,205 @@
             }
                // Check your work! Your bank_account should have $10,100 in it.
                console.log (`Bank Account : ${bank_account * 2}$.`);
-
       }
       // savingAccount();
+
+
+
+
+
+// ---------------------------------- III. Arrays & Control flow ----------------------------------------------------------------------      
+
+// A. Talk about it:
+
+      // 1. What are the things in an array called?
+         /* Elements */ 
+
+      // 2. Do Arrays guarantee those things will be in order?
+         /* Yes */
+
+      // 3. What real-life thing could you model with an array?
+         /* To-Do list */
+
+
+// B. Easy Does It
+      function easyDoesIt () {
+         // 1. Create an array that contains three quotes and store it in a variable called quotes
+         qoutes = ["A", 12, true];
+         console.log(qoutes);
+      }
+       // easyDoesIt();
+
+
+// C. Accessing elements
+      function accessingElements () {
+         // Given the following array const randomThings = [1, 10, "Hello", true].
+            const randomThings = [1, 10, "Hello", true]
+
+         // 1. How do you access the 1st element in the array?
+            console.log(randomThings[0]);
+
+         // 2. Change the value of "Hello"to "World"
+            randomThings[2] = "World";
+            console.log(randomThings);
+
+         // 3. Check the value of the array to make sure it updated the array. How? Why, yes! console.log();
+            /* Already checked console.log() */
+      }
+      // accessingElements();
+
+
+// D. Change values
+      function changeValues () {
+         // Given the following array const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"].
+            const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"];
+
+         // 1. What would you write to access the 3rd element of the array?
+            console.log(ourClass[2]);
+
+         // 2. Change the value of "Github" to "Octocat".
+            ourClass[4] = "Octocat";
+            console.log(ourClass);
+
+         // 3. Add a new element, "Cloud City" to the array.
+            ourClass.push("Cloud City");
+            console.log(ourClass);
+      }      
+      // changeValues();
+
+
+// E. Mix It Up
+      function mixItUp () {
+         // Given the following array: const myArray = [5, 10, 500, 20].
+            const myArray = [5, 10, 500, 20];
+
+         // 1. Add the string "Aegon" to the end of the array. Add another string of your choice to the end of the array.
+            myArray.push("Aegon", "Sub Zero");
+            console.log(myArray);
+
+         // 2. Remove the 5 from the beginning of the array.
+            myArray.shift(0);
+            console.log(myArray);
+
+         // 3. Add the string "Bob Marley"to the beginning of the array.
+            myArray.unshift("Bob Marley");
+            console.log(myArray);
+
+         // 4. Remove the string of your choice from the end of the array.
+            myArray.pop();
+            console.log(myArray);
+
+         // 5. Reverse this array using Array.prototype.reverse(). Did you mutate the array? What does mutate mean? Did the .reverse()method return anything?
+            myArray.reverse();
+            console.log(myArray);
+            /* The array has been reversed, all values are still there after the changes I've made. */
+      }      
+      // mixItUp();
+
+
+// F. Biggie Smalls
+      function biggieSmalls () {
+         // Create a variable that contains an integer.
+            let myVariable = 44;
+
+         // Write an if ... elsestatement that:
+         // 1. console.log()s "little number" if the number is entered is less than 100
+            if (myVariable < 100) {
+               console.log(`${myVariable} Little Number`);
+            } 
+            
+         // 2. console.log()s big numberif the number is greater than or equal to 100.
+            else if (myVariable >= 100) {
+               console.log (`${myVariable} Big Number`)
+            }
+      }  
+      // biggieSmalls();    
+
+
+// G. Monkey in the Middle
+      function monkeyInTheMiddle () {
+         // Write an if ... else if ... elsestatement:
+            let monkeyNumber = 3;
+
+         // 1. console.log()little numberif the number entered is less than 5.
+            if (monkeyNumber < 5) {
+               console.log(`${monkeyNumber} Little Number`);
+            } 
+            
+         // 2. If the number entered is more than 10, log big number.
+            else if (monkeyNumber > 10) {
+               console.log(`${monkeyNumber} Big Number`);
+            } 
+            
+         // 3. Otherwise, log "monkey".
+            else {
+               console.log("Monkey");
+            }
+      }       
+      // monkeyInTheMiddle();
+
+
+// H. What's in Your Closet?
+      function yourCloset () {
+
+         // Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.
+            const kristynsCloset = [
+               "left shoe",
+               "cowboy boots",
+               "right sock",
+               "Per Scholas hoodie",
+               "green pants",
+               "yellow knit hat",
+               "marshmallow peeps"
+            ];
+            
+            // Thom's closet is more complicated. Check out this nested data structure!!
+            const thomsCloset = [
+               [
+               // These are Thom's shirts
+               "grey button-up",
+               "dark grey button-up",
+               "light blue button-up",
+               "blue button-up",
+               ],[
+               // These are Thom's pants
+               "grey jeans",
+               "jeans",
+               "PJs"
+               ],[
+               // Thom's accessories
+               "wool mittens",
+               "wool scarf",
+               "raybans"
+               ]
+            ];
+
+
+         // 1. What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+            console.log("Kristyn is rocking that "  + kristynsCloset[2] + " today!");
+
+         // 2. Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+            kristynsCloset.splice(6,0,"raybans");
+            console.log(kristynsCloset);
+
+         // 3. Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+            kristynsCloset[5] = ("stain knit had");
+            console.log(kristynsCloset);
+
+         // 4. Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+            console.log(thomsCloset[0][0]);
+
+         // 5. In the same way, access one item from Thom's pants array.
+            console.log(thomsCloset[1][0]);
+
+         // 6. Access one item from Thom's accessories array.
+            console.log(thomsCloset[2][1]);
+         
+         // 7. Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!
+            console.log(`Thom is looking fierce in a ${thomsCloset[0][0]}, ${thomsCloset[1][1]} and ${thomsCloset[2][1]}!`);
+
+         // 8. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+            thomsCloset[1][2] = "Footie Pajamas";
+            console.log(thomsCloset);
+      }
+      // yourCloset();
